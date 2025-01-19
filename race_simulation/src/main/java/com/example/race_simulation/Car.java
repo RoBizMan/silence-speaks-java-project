27f2cff5a335fd4966b0715535841c54db1a290d
Robert Tiffert <152310537+RoBizMan@users.noreply.github.com>
2025-01-19 21:11:09 +0100

    Create Car subclass

---
+++ race_simulation/src/main/java/com/example/race_simulation/Car.java
@@ -0,0 +1,11 @@
+package race_simulation.src.main.java.com.example.race_simulation;
+
+import java.util.Random;
+
+public class Car extends Vehicle {
+    private static final Random random = new Random();
+
+    public Car(String name) {
+        super(name, random.nextInt(41) + 90, random.nextDouble() * 2 + 4, random.nextInt(1451) + 1800);
+    }
+}
