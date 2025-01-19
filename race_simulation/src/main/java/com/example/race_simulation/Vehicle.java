e5c693ba268ce0c1bdb35d28d2f9bfaaf70ed99a
Robert Tiffert <152310537+RoBizMan@users.noreply.github.com>
2025-01-19 21:06:24 +0100

    Create Race Simulation and create vehicle class

---
+++ race_simulation/src/main/java/com/example/race_simulation/Vehicle.java
@@ -0,0 +1,26 @@
+package race_simulation.src.main.java.com.example.race_simulation;
+
+public abstract class Vehicle {
+    protected String name;
+    protected double speed; // km/h
+    protected double acceleration; // m/s^2
+    protected double weight; // kg
+
+    public Vehicle(String name, double speed, double acceleration, double weight) {
+        this.name = name;
+        this.speed = speed;
+        this.acceleration = acceleration;
+        this.weight = weight;
+    }
+
+    public double raceTrack() {
+        double distance = 2000; // m
+        double realisticSpeed = speed * (1 - (weight / 10000));
+        double time = distance / (realisticSpeed / 3.6); // s
+        return time;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
